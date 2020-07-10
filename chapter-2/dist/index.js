3d113ad8c1defcc3f01ed0bca727f799f2f54258
tofu0308 <isisaka2000@gmail.com>
2020-07-10 20:51:39 +0900

    練習問題

--- chapter-2/dist/index.js
+++ chapter-2/dist/index.js
@@ -1,3 +1,18 @@
 "use strict";
 console.log('hello TypeScript');
+{
+    let a = 1 + 2;
+    let b = a + 3;
+    let c = {
+        apple: a,
+        banana: b
+    };
+    let d = c.apple * 4;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(c.apple);
+    console.log(c.banana);
+    console.log(d);
+}
 //# sourceMappingURL=index.js.map
--- chapter-2/src/index.ts
+++ chapter-2/src/index.ts
@@ -1,1 +1,20 @@
 console.log('hello TypeScript')
+
+{
+  let a: number = 1 + 2
+  let b: number = a + 3
+  let c: {apple: number, banana: number} = {
+    apple: a,
+    banana: b
+  };
+
+  let d: number = c.apple * 4
+
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(c.apple);
+  console.log(c.banana);
+  console.log(d);
+}
+
