09298c39a9a82d4e5a16846600419cf1582e1eb8
tofu0308 <isisaka2000@gmail.com>
2020-07-13 23:00:19 +0900

    関数squareOfのnをnumberに制約

---
+++ chapter-3/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function squareOf(n) {
+    return n * n;
+}
+// number以外はエラー
+console.log(squareOf(2 + 2));
+//# sourceMappingURL=index.js.map
---
+++ chapter-3/src/index.ts
@@ -0,0 +1,6 @@
+function squareOf(n:number) {
+  return n * n
+}
+
+// number以外はエラー
+console.log(squareOf(2+2))
