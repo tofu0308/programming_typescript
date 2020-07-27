02d9c0f751a1fade365eb198bb750756494b56b9
tofu0308 <isisaka2000@gmail.com>
2020-07-27 20:18:34 +0900

    unknown

--- chapter-3/dist/index.js
+++ chapter-3/dist/index.js
@@ -4,11 +4,29 @@ function squareOf(n) {
 }
 // number以外はエラー
 console.log(squareOf(2 + 2));
+console.log('ここから3.2.1　型の初歩');
 // any
-const a = 666;
-const b = ['any', 'string'];
-const c = a + b;
-console.log(a);
-console.log(b);
-console.log(c);
+console.log('any ///////');
+{
+    const a = 666;
+    const b = ['any', 'string'];
+    const c = a + b;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+// unknown
+console.log('unknown ///////');
+{
+    const a = 30;
+    const b = a === 123;
+    // const c = a + 10; => エラー
+    let d;
+    if (typeof a === 'number') {
+        d = a + 10;
+    }
+    console.log(a);
+    console.log(b);
+    console.log(d);
+}
 //# sourceMappingURL=index.js.map
--- chapter-3/src/index.ts
+++ chapter-3/src/index.ts
@@ -5,11 +5,31 @@ function squareOf(n:number) {
 // number以外はエラー
 console.log(squareOf(2+2))
 
+console.log('ここから3.2.1　型の初歩');
 // any
-const a: any = 666;
-const b: any = ['any', 'string'];
-const c: any = a + b;
+console.log('any ///////');
+{
+  const a: any = 666;
+  const b: any = ['any', 'string'];
+  const c: any = a + b;
 
-console.log(a);
-console.log(b);
-console.log(c);
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+// unknown
+console.log('unknown ///////');
+{
+  const a: unknown = 30;
+  const b = a === 123;
+  // const c = a + 10; => エラー
+
+  let d;
+  if (typeof a === 'number') {
+    d = a +10;
+  }
+  console.log(a);
+  console.log(b);
+  console.log(d);
+
+}
