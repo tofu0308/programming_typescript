3e67ee2ece3bffb20876a61b7a01b957d87a5f09
tofu0308 <isisaka2000@gmail.com>
2020-07-27 20:08:17 +0900

    any

--- chapter-3/dist/index.js
+++ chapter-3/dist/index.js
@@ -4,4 +4,11 @@ function squareOf(n) {
 }
 // number以外はエラー
 console.log(squareOf(2 + 2));
+// any
+const a = 666;
+const b = ['any', 'string'];
+const c = a + b;
+console.log(a);
+console.log(b);
+console.log(c);
 //# sourceMappingURL=index.js.map
--- chapter-3/src/index.ts
+++ chapter-3/src/index.ts
@@ -4,3 +4,12 @@ function squareOf(n:number) {
 
 // number以外はエラー
 console.log(squareOf(2+2))
+
+// any
+const a: any = 666;
+const b: any = ['any', 'string'];
+const c: any = a + b;
+
+console.log(a);
+console.log(b);
+console.log(c);
