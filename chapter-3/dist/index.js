c8ad18cdc11e0f5a090113867184aa9395b8b086
tofu0308 <isisaka2000@gmail.com>
2020-07-27 20:35:43 +0900

    number

--- chapter-3/dist/index.js
+++ chapter-3/dist/index.js
@@ -20,7 +20,7 @@ console.log('unknown ///////');
 {
     const a = 30;
     const b = a === 123;
-    // const c = a + 10; => エラー
+    // const c = a + 10; エラー
     let d;
     if (typeof a === 'number') {
         d = a + 10;
@@ -37,11 +37,28 @@ console.log('boolean ///////');
     const c = true;
     let d = true;
     let e = true;
-    // let f: false = true; => エラー
+    // let f: false = true; エラー
     console.log(a);
     console.log(b);
-    console.log(typeof c);
+    console.log(c);
     console.log(d);
     console.log(e);
+}
+// number
+console.log('number ///////');
+{
+    let a = 1234;
+    let b = Infinity * 0.10;
+    const c = 5678;
+    let d = a < b;
+    let e = 100;
+    let f = 26.18;
+    // let g: 26.18 = 26; エラー
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+    console.log(e);
+    console.log(f);
 }
 //# sourceMappingURL=index.js.map
