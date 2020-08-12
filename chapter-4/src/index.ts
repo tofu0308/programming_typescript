5d00d0699893bd96828d8c1c11da6067afe66733
tofu0308 <isisaka2000@gmail.com>
2020-08-12 20:47:54 +0900

    オプションパラメーターとデフォルトパラメーター

--- chapter-4/src/index.ts
+++ chapter-4/src/index.ts
@@ -34,3 +34,36 @@
   // console.log(add(1)); 2 個の引数が必要ですが、1 個指定されました。ts(2554)
   // console.log(add(1, 'a')); 型 '"a"' の引数を型 'number' のパラメーターに割り当てることはできません。ts(2345)
 }
+
+// オプションパラメーターとデフォルトパラメーター
+{
+  function log(message: string, userId?: string) {
+    let time = new Date().toLocaleTimeString()
+    console.log(time, message, userId || 'Not signed in, pattern A.');
+  }
+
+  log('Page loaded');
+  log('User signed in', '123abc');
+}
+{
+  function log(message: string, userId = 'Not signed in, pattern B.') {
+    let time = new Date().toLocaleTimeString()
+    console.log(time, message, userId);
+  }
+
+  log('User clicked on a button', 'def456');
+  log('User, signed out.');
+  
+}
+{
+  type Context = {
+    appId?: string
+    userId?: string
+  }
+
+  function log(message: string, context: Context = {}) {
+    let time = new Date().toLocaleTimeString();
+    console.log(time, message, context.userId);
+  }
+  log('patter3', {userId: '7g8h9i'})
+}
