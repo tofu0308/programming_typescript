efe6f33bdec4a2516b94fd9a5837beef1de48315
takeshi.fujii <isisaka3000@gmail.com>
2020-10-10 13:12:38 +0900

    classと継承

--- chapter-5/dist/index.js
+++ chapter-5/dist/index.js
@@ -1,2 +1,48 @@
 "use strict";
+// クラスと継承
+{
+    // チェスのゲームを表します
+    class Game {
+        constructor() {
+            this.pieces = Game.makePieces();
+        }
+        static makePieces() {
+            return [
+                new King('White', 'E', 1),
+                new King('Black', 'E', 8),
+            ];
+        }
+    }
+    // チェスの駒
+    class Piece {
+        constructor(color, file, rank) {
+            this.color = color;
+            this.position = new Position(file, rank);
+        }
+        moveTo(position) {
+            this.position = this.position;
+        }
+    }
+    // 駒の位置（座標）
+    class Position {
+        constructor(file, rank) {
+            this.file = file;
+            this.rank = rank;
+        }
+        distanceFrom(position) {
+            return {
+                rank: Math.abs(position.rank - this.rank),
+                file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0))
+            };
+        }
+    }
+    // 各駒
+    class King extends Piece {
+        canMoveTo(position) {
+            let distance = this.position.distanceFrom(position);
+            return distance.rank < 2 && distance.file < 2;
+        }
+    }
+    // new Piece('White', 'E', 1) 抽象クラスのインスタンスは作成できません。ts(2511)
+}
 //# sourceMappingURL=index.js.map
---
+++ chapter-5/src/index.ts
@@ -0,0 +1,82 @@
+// クラスと継承
+{
+  // チェスのゲームを表します
+  class Game {
+    private pieces = Game.makePieces()
+
+    private static makePieces() {
+      return [
+        new King('White', 'E', 1),
+        new King('Black', 'E', 8),
+
+        /*
+        new Queen('White', 'D', 1),
+        new Queen('Black', 'D', 8),
+
+        new Bishop('White', 'C', 1),
+        new Bishop('Black', 'F', 1),
+        new Bishop('White', 'C', 8),
+        new Bishop('Black', 'F', 8),
+        */
+        // .....
+      ]
+    }
+  }
+  
+  // チェスの駒
+  abstract class Piece {
+    protected position: Position
+    constructor(
+      private readonly color: Color,
+      file: File,
+      rank: Rank
+    ){
+      this.position = new Position(file, rank)
+    }
+
+    moveTo(position: Position) {
+      this.position = this.position
+    }
+    abstract canMoveTo(position: Position): boolean
+  }
+
+  // 駒の位置（座標）
+  class Position {
+    constructor(
+      private file: File,
+      private rank: Rank
+    ){}
+
+    distanceFrom(position: Position) {
+      return  {
+        rank: Math.abs(position.rank - this.rank),
+        file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0))
+      }
+    }
+  }
+
+  // 各駒
+  class King extends Piece{
+    canMoveTo(position: Position) {
+      let distance = this.position.distanceFrom(position)
+      return distance.rank < 2 && distance.file < 2
+    }
+  }
+
+  // 非抽象クラス 'Bishop' はクラス 'Piece' からの継承抽象メンバー 'canMoveTo' を実装しません。ts(2515)
+  /*
+  class Queen extends Piece{}　
+  class Bishop extends Piece{}
+  class Knight extends Piece{}
+  class Rook extends Piece{}
+  class Pawn extends Piece{}
+  */
+
+  type Color = 'Black'|'White'
+  type File = 'A'|'B'|'C'|'D'|'E'|'F'|'G'|'H'
+  type Rank = 1|2|3|4|5|6|7|8
+
+  // new Piece('White', 'E', 1) 抽象クラスのインスタンスは作成できません。ts(2511)
+
+
+}
