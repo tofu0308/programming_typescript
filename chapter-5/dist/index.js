6a4821f586f28352b83d57804145ebe3b91c7700
takeshi.fujii <isisaka3000@gmail.com>
2020-11-04 19:19:53 +0900

    デザインパターン：ファクトリーパターン

--- chapter-5/dist/index.js
+++ chapter-5/dist/index.js
@@ -293,4 +293,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
     let messageQueue = MessageQueue.create([]);
     console.log(messageQueue);
 }
+// デザインパターン
+// ファクトリーパターン
+{
+    class BalletFlat {
+        constructor() {
+            this.purpose = 'dancing';
+        }
+    }
+    class Boot {
+        constructor() {
+            this.purpose = 'woodcutting';
+        }
+    }
+    class Sneaker {
+        constructor() {
+            this.purpose = 'walking';
+        }
+    }
+    let Shoe = {
+        create(type) {
+            switch (type) {
+                case 'balletFlat': return new BalletFlat;
+                case 'boot': return new Boot;
+                case 'sneaker': return new Sneaker;
+            }
+        }
+    };
+    console.log(Shoe.create('balletFlat'));
+    console.log(Shoe.create('boot'));
+    console.log(Shoe.create('sneaker'));
+}
 //# sourceMappingURL=index.js.map
--- chapter-5/src/index.ts
+++ chapter-5/src/index.ts
@@ -437,3 +437,37 @@ import { serialize } from "v8"
   let messageQueue = MessageQueue.create([])
   console.log(messageQueue)
  }
+
+ // デザインパターン
+ // ファクトリーパターン
+ {
+   type Shoe = {
+     purpose: string
+   }
+
+   class BalletFlat implements Shoe {
+    purpose = 'dancing'
+   }
+
+   class Boot implements Shoe {
+     purpose = 'woodcutting'
+   }
+
+   class Sneaker implements Shoe {
+     purpose = 'walking'
+   }
+
+   let Shoe = {
+     create(type: 'balletFlat' | 'boot' | 'sneaker'): Shoe {
+       switch(type) {
+         case 'balletFlat': return new BalletFlat
+         case 'boot': return new Boot
+         case 'sneaker': return new Sneaker
+       }
+     }
+   }
+  
+   console.log(Shoe.create('balletFlat'))
+   console.log(Shoe.create('boot'))
+   console.log(Shoe.create('sneaker'))
+}
