6d09b95ae9e4b71a9324ee85f8cd8f286105f489
takeshi.fujii <isisaka3000@gmail.com>
2020-11-12 21:24:21 +0900

    関数の変性

--- chapter-6/dist/index.js
+++ chapter-6/dist/index.js
@@ -24,4 +24,60 @@
           型 'string' を型 'number | undefined' に割り当てることはできません。ts(2345)
     */
 }
+{
+    class Animal {
+    }
+    class Bird extends Animal {
+        chirp() { }
+    }
+    class Crow extends Bird {
+        caw() { }
+    }
+    /*
+      CrowはBirdのサブタイプ
+      BirdはAnimalsのサブタイプ
+      Crow <: Bird <: Annimals
+    */
+    function chirp(bird) {
+        bird.chirp();
+        return bird;
+    }
+    // chirp(new Animal)
+    /*
+      型 'Animal' の引数を型 'Bird' のパラメーターに割り当てることはできません。
+      プロパティ 'chirp' は型 'Animal' にありませんが、型 'Bird' では必須です。ts(2345)
+    */
+    chirp(new Bird);
+    chirp(new Crow);
+    function clone(f) { }
+    // 宣言された型が 'void' でも 'any' でもない関数は値を返す必要があります。ts(2355)  に暫定対処
+    // @ts-ignore
+    function birdToBird(b) { }
+    clone(birdToBird);
+    // 宣言された型が 'void' でも 'any' でもない関数は値を返す必要があります。ts(2355)  に暫定対処
+    // @ts-ignore
+    function birdToCrow(d) { }
+    clone(birdToCrow);
+    // 宣言された型が 'void' でも 'any' でもない関数は値を返す必要があります。ts(2355)  に暫定対処
+    // @ts-ignore
+    function birdToAnimal(d) { }
+    // clone(birdToAnimal)
+    /*
+    型 '(d: Bird) => Animal' の引数を型 '(b: Bird) => Bird' のパラメーターに割り当てることはできません。
+    プロパティ 'chirp' は型 'Animal' にありませんが、型 'Bird' では必須です。ts(2345)
+    */
+    // 宣言された型が 'void' でも 'any' でもない関数は値を返す必要があります。ts(2355)  に暫定対処
+    // @ts-ignore
+    function animalToBird(a) { }
+    clone(animalToBird);
+    // 宣言された型が 'void' でも 'any' でもない関数は値を返す必要があります。ts(2355)  に暫定対処
+    // @ts-ignore
+    function crowToBird(c) { }
+    // clone(crowToBird)
+    /*
+      型 '(c: Crow) => Bird' の引数を型 '(b: Bird) => Bird' のパラメーターに割り当てることはできません。
+        パラメーター 'c' および 'b' は型に互換性がありません。
+          プロパティ 'caw' は型 'Bird' にありませんが、型 'Crow' では必須です。ts(2345)
+    */
+}
 //# sourceMappingURL=index.js.map
--- chapter-6/src/index.ts
+++ chapter-6/src/index.ts
@@ -39,3 +39,67 @@ type LegacyUser = {
         型 'string' を型 'number | undefined' に割り当てることはできません。ts(2345) 
   */
 }
+{
+  class Animal {}
+  class Bird extends Animal {
+    chirp(){}
+  }
+  class Crow extends Bird {
+    caw():void {}
+  }
+  /*
+    CrowはBirdのサブタイプ
+    BirdはAnimalsのサブタイプ
+    Crow <: Bird <: Annimals
+  */
+
+  function chirp(bird: Bird): Bird {
+    bird.chirp()
+    return bird
+  }
+
+  // chirp(new Animal)
+  /*
+    型 'Animal' の引数を型 'Bird' のパラメーターに割り当てることはできません。
+    プロパティ 'chirp' は型 'Animal' にありませんが、型 'Bird' では必須です。ts(2345)
+  */
+
+  chirp(new Bird)
+  chirp(new Crow)
+
+  function clone(f: (b: Bird) => Bird): void {}
+
+  // 宣言された型が 'void' でも 'any' でもない関数は値を返す必要があります。ts(2355)  に暫定対処
+  // @ts-ignore
+  function birdToBird(b: Bird): Bird {}
+  clone(birdToBird)
+
+  // 宣言された型が 'void' でも 'any' でもない関数は値を返す必要があります。ts(2355)  に暫定対処
+  // @ts-ignore
+  function birdToCrow(d: Bird): Crow {}
+  clone(birdToCrow)
+
+  // 宣言された型が 'void' でも 'any' でもない関数は値を返す必要があります。ts(2355)  に暫定対処
+  // @ts-ignore
+　function birdToAnimal(d: Bird): Animal{}
+  // clone(birdToAnimal)
+  /*
+  型 '(d: Bird) => Animal' の引数を型 '(b: Bird) => Bird' のパラメーターに割り当てることはできません。
+  プロパティ 'chirp' は型 'Animal' にありませんが、型 'Bird' では必須です。ts(2345)
+  */
+
+  // 宣言された型が 'void' でも 'any' でもない関数は値を返す必要があります。ts(2355)  に暫定対処
+  // @ts-ignore
+  function animalToBird(a: Animal):Bird {}
+  clone(animalToBird)
+
+  // 宣言された型が 'void' でも 'any' でもない関数は値を返す必要があります。ts(2355)  に暫定対処
+  // @ts-ignore
+  function crowToBird(c: Crow):Bird {}
+  // clone(crowToBird)
+  /*
+    型 '(c: Crow) => Bird' の引数を型 '(b: Bird) => Bird' のパラメーターに割り当てることはできません。
+      パラメーター 'c' および 'b' は型に互換性がありません。
+        プロパティ 'caw' は型 'Bird' にありませんが、型 'Crow' では必須です。ts(2345) 
+  */
+}
