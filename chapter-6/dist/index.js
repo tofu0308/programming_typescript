3d8197dbe12f1e6e93a6a365c264631a2154d2cb
takeshi.fujii <isisaka3000@gmail.com>
2020-11-24 23:09:27 +0900

    constアサーション

--- chapter-6/dist/index.js
+++ chapter-6/dist/index.js
@@ -129,4 +129,22 @@
     // 宣言されたスコープを離れると明確な型を割り当てる
     x(); // string
 }
+{
+    // constアサーション
+    let a = { x: 3 }; // x: number
+    let b; // x: 3
+    let c = { x: 3 }; // readonly x: 3;
+    let d = [1, { x: 2 }];
+    /*
+      let d: (number | {
+        x: number;
+      })[]
+    */
+    let e = [1, { x: 2 }];
+    /*
+      let e: readonly [1, {
+        readonly x: 2;
+      }]
+    */
+}
 //# sourceMappingURL=index.js.map
--- chapter-6/src/index.ts
+++ chapter-6/src/index.ts
@@ -158,4 +158,26 @@ type LegacyUser = {
   // 宣言されたスコープを離れると明確な型を割り当てる
   x() // string
 
+
+}
+
+{
+  // constアサーション
+  let a = {x: 3} // x: number
+  let b: {x: 3} // x: 3
+  let c = {x: 3} as const // readonly x: 3;
+
+  let d = [1, {x: 2}]
+  /*
+    let d: (number | {
+      x: number;
+    })[]
+  */
+
+  let e = [1, {x: 2}] as const 
+  /*
+    let e: readonly [1, {
+      readonly x: 2;
+    }]
+  */
 }
