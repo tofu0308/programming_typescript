da057f88fac341f588726c905b742eb6903c0373
takeshi.fujii <isisaka3000@gmail.com>
2020-12-08 21:04:00 +0900

    例外をスローする

--- chapter-7/src/index.ts
+++ chapter-7/src/index.ts
@@ -27,3 +27,47 @@
     console.error('Error parsing date for some reason')
   }
 }
+
+// 例外をスローする
+{
+  function ask() {
+    return null
+  }
+
+  class InvalidDateFormatError extends RangeError {}
+  class DateIsInTheFutureError extends RangeError {}
+  
+  // 挙動確認のためanyに変更
+  function parse(birthday: any): Date|null {
+    let date = new Date(birthday)
+    if(!isValid(date)) {
+      throw new InvalidDateFormatError('Enter a date in the form YYY/MM/DD')
+    }
+    if (date.getTime() > Date.now()) {
+      throw new DateIsInTheFutureError('Are you a timeload?')
+    }
+    return date
+  }
+ 
+    // 与えられた日付が有効かどうかをチェック
+    function isValid(date: Date) {
+      return Object.prototype.toString.call(date) === '[object Date]' && !Number.isNaN(date.getTime())
+    }
+
+    try {
+      let date = parse(ask())
+      // オプショナルチェイニングでエラー避け
+      console.info(`Date is ${date?.toISOString()}`)
+
+    } catch(e) {
+     if(e instanceof InvalidDateFormatError) {
+      console.error(e.message)
+
+     } else if(e instanceof DateIsInTheFutureError) {
+      console.error(e.message)
+
+     } else {
+      throw e
+     }
+   }
+}
