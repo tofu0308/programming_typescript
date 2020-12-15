781542458d616b8a5e25fa9b9689cdb7a10bd8b3
takeshi.fujii <isisaka3000@gmail.com>
2020-12-15 19:50:27 +0900

    javascriptのイベントループ

---
+++ chapter-8/dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+// 非同期プログラミングと並行処理
+// Javascriptのイベントループ　
+{
+    // 実行結果はC>B>A 邪魔なのでコメントにする
+    /*
+    setTimeout(()=>console.info('A')  ,1)
+    setTimeout(()=>console.info('B')  ,2)
+    console.info('C')
+    */
+}
+// コールバックの処理
+{
+}
+//# sourceMappingURL=index.js.map
