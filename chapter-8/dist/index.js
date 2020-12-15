c0038c5cda6b28d1616dcf22a8afeba616edfa33
takeshi.fujii <isisaka3000@gmail.com>
2020-12-15 20:04:14 +0900

    コールバックの処理

--- chapter-8/dist/index.js
+++ chapter-8/dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // 非同期プログラミングと並行処理
+const fs = require("fs");
 // Javascriptのイベントループ　
 {
     // 実行結果はC>B>A 邪魔なのでコメントにする
@@ -11,5 +13,18 @@
 }
 // コールバックの処理
 {
+    function readFile(path, options, callback) { }
+    fs.readFile('/var/log/apache2/access_log', { encoding: 'utf8' }, (error, data) => {
+        if (error) {
+            console.error('error reading!', error);
+            return;
+        }
+        console.log('success reading!', data);
+    });
+    fs.appendFile('/var/log/apache2/access_log', 'New access log entry.', error => {
+        if (error) {
+            console.error('error writeig!', error);
+        }
+    });
 }
 //# sourceMappingURL=index.js.map
--- chapter-8/src/index.ts
+++ chapter-8/src/index.ts
@@ -1,4 +1,6 @@
 // 非同期プログラミングと並行処理
+import * as fs from 'fs'
+
 // Javascriptのイベントループ　
 {
   // 実行結果はC>B>A 邪魔なのでコメントにする
@@ -11,5 +13,32 @@
 
 // コールバックの処理
 {
-  
+  function readFile(
+    path: string,
+    options: {encoding: string, flag?: string},
+    callback: (err: Error|null, data:string|null) => void
+  ): void {}
+
+  fs.readFile(
+    '/var/log/apache2/access_log',
+    {encoding: 'utf8'},
+    (error, data) => {
+      if(error) {
+        console.error('error reading!', error)
+        return
+      }
+      console.log('success reading!', data)
+    }
+  )
+
+  fs.appendFile(
+    '/var/log/apache2/access_log',
+    'New access log entry.',
+    error => {
+      if(error) {
+        console.error('error writeig!', error)
+      }
+    }
+  )
+
 }
