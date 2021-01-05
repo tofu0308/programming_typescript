8db8b4e860b35de1dd393ff50150e0988c797d7e
takeshi.fujii <isisaka3000@gmail.com>
2021-01-05 22:30:51 +0900

    reactの実装　とhtmlへのレンダリングまで

--- chapter-9/src/index.tsx
+++ chapter-9/src/index.tsx
@@ -1,4 +1,44 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import {FancyButton} from './FancyButton'
 
+type Props = {
+  firstName: string
+  userId: string
+}
 
+type State = {
+  isLoading: boolean
+}
+
+class SignupForm extends React.Component<Props, State>{
+  state = {
+    isLoading: false
+  }
+  render() {
+    return (
+    <React.Fragment>
+      <h2>Sign up for a 7-day supply of our tasty toothpaste now, {this.props.firstName}</h2>
+      <FancyButton
+        isDisabled={this.state.isLoading}
+        size='Big'
+        text='Sign up now'
+        onClick={this.signUp}
+      />
+    </React.Fragment>  
+    )
+  }
+
+  private signUp = async() => {
+    this.setState({isLoading: true})
+    try {
+      await fetch(`/api/signup>userID=${this.props.userId}`)
+    } finally {
+      this.setState({isLoading: false})
+    }
+  }
+}
+
+let form = <SignupForm firstName='firstname san' userId='1234' />
+
+ReactDOM.render(form, document.querySelector('#app'))
