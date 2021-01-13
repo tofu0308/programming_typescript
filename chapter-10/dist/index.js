df3c49b15bc72753be58b6a5813f760cc8b9bb73
takeshi.fujii <isisaka3000@gmail.com>
2021-01-14 06:04:54 +0900

    namespace

--- chapter-10/dist/index.js
+++ chapter-10/dist/index.js
@@ -10,4 +10,40 @@ function getUserLocale() {
     return 'us';
 }
 main();
+// 名前空間
+// 複数のファイルにまたがって1つの名前空間を設定することが可能（ここでは一つのファイルにまとめている）
+var Network;
+(function (Network) {
+    function get(url) {
+        console.log(url);
+        return url;
+    }
+    Network.get = get;
+    let HTTP;
+    (function (HTTP) {
+        function get(url) {
+            console.log(url);
+            return url;
+        }
+        HTTP.get = get;
+    })(HTTP = Network.HTTP || (Network.HTTP = {}));
+})(Network || (Network = {}));
+var App;
+(function (App) {
+    Network.get('https://www.google.com/');
+})(App || (App = {}));
+// 名前空間エイリアス
+var A;
+(function (A) {
+    let B;
+    (function (B) {
+        let C;
+        (function (C) {
+            C.d = 3;
+        })(C = B.C || (B.C = {}));
+    })(B = A.B || (A.B = {}));
+})(A || (A = {}));
+var d = A.B.C.d;
+let e = d * 3;
+console.log(e);
 //# sourceMappingURL=index.js.map
--- chapter-10/src/index.ts
+++ chapter-10/src/index.ts
@@ -1,4 +1,6 @@
 import {locale} from './locales/locale-us'
+import { SSL_OP_NETSCAPE_DEMO_CIPHER_CHANGE_BUG } from 'constants'
+import { promises } from 'fs'
 
 async function main() {
   let userLocale = await getUserLocale()
@@ -13,3 +15,45 @@ function getUserLocale() {
 }
 
 main()
+
+// 名前空間
+// 複数のファイルにまたがって1つの名前空間を設定することが可能（ここでは一つのファイルにまとめている）
+namespace Network {
+  export function get<T>(url: string): string {
+    console.log(url)
+    return url
+  }
+  export namespace HTTP {
+    export function get<T>(url: string): string {
+      console.log(url)
+      return url
+    }
+  }
+
+  export namespace TCP {
+    // ...
+  }
+  export namespace UDP {
+    // ...
+  }
+  export namespace IP {
+    // ...
+  }
+}
+
+namespace App {
+  Network.get<string>('https://www.google.com/')
+}
+
+// 名前空間エイリアス
+namespace A {
+  export namespace B {
+    export namespace C {
+      export let d = 3
+    }
+  }
+}
+
+import d = A.B.C.d
+let e = d*3
+console.log(e)
